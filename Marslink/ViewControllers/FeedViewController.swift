497de0b3624b846631ac4233d975cc39602aa4e6
CarolineCruz <cruz.caroline08@gmail.com>
2019-04-11 18:10:41 -0400

    Created new FeedViewController and began adding collectionView

--- Marslink/ViewControllers/FeedViewController.swift
+++ Marslink/ViewControllers/FeedViewController.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2018 Razeware LLC
-///
+/// Copyright (c) 2019 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,18 +27,29 @@
 /// THE SOFTWARE.
 
 import UIKit
+import IGListKit
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-  var window: UIWindow?
-  
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    window = UIWindow(frame: UIScreen.main.bounds)
-    window?.backgroundColor = .black
-    let nav = UINavigationController(navigationBarClass: CustomNavigationBar.self, toolbarClass: nil)
-    nav.pushViewController(FeedViewController(), animated: false)
-    window?.rootViewController = nav
-    window?.makeKeyAndVisible()
-    return true
-  }
+class FeedViewController: UIViewController {
+
+    let loader = JournalEntryLoader()
+    let collectionView: UICollectionView = {
+//        start with a zero-sized rect since view isnt created yet
+        let view = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loader.loadLatest()
+        view.addSubview(collectionView)
+    }
+    
+//    this overrides viewDidLayoutSubviews(), setting the collectionView frame to match the view bounds.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
+
+    
 }
